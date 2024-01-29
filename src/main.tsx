237413812590be035cf067d6d60844441a175ba4
fltzr <josh.salinas.j@outlook.com>
2024-01-28 20:29:58 -0500

    chore: add csd global-styles to root

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { RouterProvider } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { router } from '@/common/router/routes';
 
+import '@cloudscape-design/global-styles/index.css';
 import './index.css';
 
 const container: HTMLElement | null = document.querySelector('#c');
