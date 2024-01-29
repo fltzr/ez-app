329adb758c947dd3fd7baf6ca02a03e56512fea9
fltzr <josh.salinas.j@outlook.com>
2024-01-29 00:57:47 -0500

    chore: lint

--- src/features/auth/pages/signin/index.tsx
+++ src/features/auth/pages/signin/index.tsx
@@ -5,11 +5,11 @@ import Input from '@cloudscape-design/components/input';
 import styles from './styles.module.scss';
 
 const SignInPage = () => (
-    <div className={styles['auth-form']}>
-      <Container header={<Header variant="h1">Sign in</Header>}>
-        <Input value="" placeholder="username" />
-      </Container>
-    </div>
-  );
+  <div className={styles['auth-form']}>
+    <Container header={<Header variant="h1">Sign in</Header>}>
+      <Input value="" placeholder="username" />
+    </Container>
+  </div>
+);
 
 export const Component = SignInPage;
