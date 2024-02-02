d0df6a5e49803d1a55a56f70eb4cde0fe82009a5
fltzr <josh.salinas.j@outlook.com>
2024-02-02 00:26:11 -0500

    Update language messages to use English locale

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { I18nProvider } from '@cloudscape-design/components/i18n';
-import messages from '@cloudscape-design/components/i18n/messages/all.all';
+import messages from '@cloudscape-design/components/i18n/messages/all.en';
 import { ThemeAndStyleProvider } from '@/components/theme-and-style-provider';
 
 const queryClient = new QueryClient();
