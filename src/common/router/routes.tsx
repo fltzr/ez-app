602196d452f6242e9c207fbdced52a864a544603
fltzr <josh.salinas.j@outlook.com>
2024-02-11 23:49:06 -0500

    chore: add auth components to routes

--- src/common/router/routes.tsx
+++ src/common/router/routes.tsx
@@ -1,43 +1,45 @@
-import {
-  type RouteObject,
-  createBrowserRouter,
-  Navigate,
-} from "react-router-dom";
-import { AuthWrapper } from "@/auth/auth-wrapper";
-import { ProtectedRoute } from "@/components/protected-route";
-import { RouteError } from "@/components/route-error";
+import { type RouteObject, createBrowserRouter, Navigate } from 'react-router-dom';
+import { App } from '@/app/index';
+import { AuthenticatedRoute } from '@/auth/authenticated-route';
+import { UnauthenticatedRoute } from '@/auth/unauthenticated-route';
+import { RouteError } from '@/components/route-error';
 
 const routes: RouteObject[] = [
   {
-    element: <AuthWrapper />,
+    element: <App />,
     errorElement: <RouteError />,
     children: [
       {
-        path: "/",
-        element: <Navigate replace to="/home" />,
+        path: '/',
+        element: <Navigate replace to='/home' />,
       },
       {
-        path: "signin",
-        lazy: () => import("@/features/auth/pages/signin"),
-      },
-      {
-        path: "signup",
-      },
-      {
-        path: "register",
-      },
-      {
-        element: <ProtectedRoute />,
+        element: <AuthenticatedRoute />,
         errorElement: <RouteError />,
         children: [
           {
             index: true,
-            path: "home",
-            lazy: () => import("@/features/home"),
+            path: 'home',
+            lazy: () => import('@/features/home'),
           },
           {
-            path: "courtreserve",
-            lazy: () => import("@/features/courtreserve/pages/events"),
+            path: 'courtreserve',
+            lazy: () => import('@/features/courtreserve/pages/events'),
+          },
+        ],
+      },
+      {
+        element: <UnauthenticatedRoute />,
+        children: [
+          {
+            path: 'signin',
+            lazy: () => import('@/features/auth/pages/signin'),
+          },
+          {
+            path: 'signup',
+          },
+          {
+            path: 'register',
           },
         ],
       },
