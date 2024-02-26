14b42c43b1c6c6174cf18d3d88ab36cf601beefe
fltzr <josh.salinas.j@outlook.com>
2024-02-25 23:49:34 -0500

    Add useAuthInterceptor hook to App component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import { useAuthInterceptor } from '@/auth/hooks/use-auth-interceptor';
 import Layout from '@/components/layout';
 import { useUserPreferencesEffects } from '@/hooks/use-user-preferences-effects';
 
 export const App = () => {
+  useAuthInterceptor();
   useUserPreferencesEffects();
 
   return (
